f6d6d76e6565d96511a4a333b236b282479603d6
Jesse Goodell <jessegoodell@gmail.com>
2015-10-16 15:20:15 +0000

    added GPIO orders for firing and motor

--- motion_tracking.py
+++ motion_tracking.py
@@ -1,15 +1,22 @@
-import argparse
-import datetime
-import time
 import cv2
 import numpy as np
+import RPi.GPIO as GPIO
+import time
 
 camera = cv2.VideoCapture(0)
+GPIO.setmode(GPIO.BCM)
 
 target = (460, 236)
 
+fire_pin = 23
+motor_pin = 17
+fire_displacement = 50
+
 firstFrame = None
 previousFrame = None
+
+GPIO.setup(fire_pin, GPIO.OUT)
+GPIO.setup(motor_pin, GPIO.OUT)
 
 counter = 0
 interval = 2
@@ -31,19 +38,28 @@ while(True):
     thresh = cv2.dilate(thresh, None, iterations=2)
     (contours, _) = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
                                      cv2.CHAIN_APPROX_SIMPLE)
+    firing = False
 
+    if not contours:
+        GPIO.output(motor_pin, GPIO.LOW)
+    else:
+        GPIO.output(motor_pin, GPIO.HIGH)
     for c in contours:
         if cv2.contourArea(c) < 3500:
             continue
         hasContour = True
         (x,y,w,h) = cv2.boundingRect(c)
         center = (x + w /2, y + h/4)
-        print center
         cv2.rectangle(frame, (x,y), (x + w, y + h), (0, 255, 0), 2)
         cv2.circle(frame, center, 10, (0,0,255), thickness=2)
         difference = np.sqrt((target[0] - center[0])**2 + (target[1] - center[1])**2)
-        if (difference < 25):
-            print 'firing' 
+        if (difference < fire_displacement):
+            firing = True
+
+    if firing:
+        GPIO.output(fire_pin, GPIO.HIGH)
+    else:
+        GPIO.output(fire_pin, GPIO.LOW)
 
     if previousFrame is not None:
         previousDelta = cv2.absdiff(previousFrame, gray)
@@ -61,7 +77,10 @@ while(True):
         break
     elif cmd == ord('k'):
         firstFrame = gray
+
     previousFrame = gray    
     counter += 1
+
+GPIO.cleanup()
 camera.release()
 cv2.destroyAllWindows()
